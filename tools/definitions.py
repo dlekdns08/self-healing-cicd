4c8bbccc10bd73d1bb0869870435a2e3ffe711de
dlekdns08 <dlekdns08@gmail.com>
2026-04-03 16:47:57 +0900

    apply_patches_batch 도구 추가: TOOL 목록에 apply_patches_batch 추가

--- tools/definitions.py
+++ tools/definitions.py
@@ -3,10 +3,22 @@
 """
 from tools.run_shell import run_shell
 from tools.read_file import read_file
-from tools.apply_patch import apply_patch
+from tools.apply_patch import apply_patch, apply_patches_batch
 from tools.security_scan import security_scan
 from tools.git_push import git_commit_push
-from tools.pipeline import re_trigger_pipeline
+from tools.create_fix_pr import create_fix_pr
+from tools.pipeline import re_trigger_pipeline, check_pipeline_status
 from tools.rollback import rollback_commit
 
-TOOLS = [run_shell, read_file, apply_patch, security_scan, git_commit_push, re_trigger_pipeline, rollback_commit]
+TOOLS = [
+    run_shell,
+    read_file,
+    apply_patch,
+    apply_patches_batch,
+    security_scan,
+    git_commit_push,
+    create_fix_pr,
+    re_trigger_pipeline,
+    check_pipeline_status,
+    rollback_commit,
+]
